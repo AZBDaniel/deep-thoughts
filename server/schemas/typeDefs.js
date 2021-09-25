8e0cb48a63726677569b90a1e8dd98835f438938
Bryan Daniel <brdaniel117@gmail.com>
2021-09-25 14:10:55 -0700

    completed lesson 3

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,11 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
@@ -33,11 +38,6 @@ const typeDefs = gql`
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
-
-  type Auth {
-    token: ID!
-    user: User
-  } 
 
   type Mutation {
     login(email: String!, password: String!): Auth
